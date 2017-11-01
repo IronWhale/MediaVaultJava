ca77db1203e878bd94b933c77a6503de37b5d533
IronWhale <ironwhaledev@gmail.com>
2017-11-01 02:56:07 -0400

    Worked on the Frame

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
+import java.awt.BorderLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JPopupMenu;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Main {
 
-	private JFrame frame;
+	private JFrame frmMediaVault;
 
 	/**
 	 * Launch the application.
@@ -14,7 +20,7 @@ public class Main {
 			public void run() {
 				try {
 					Main window = new Main();
-					window.frame.setVisible(true);
+					window.frmMediaVault.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -33,9 +39,19 @@ public class Main {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmMediaVault = new JFrame();
+		frmMediaVault.setTitle("Media Vault");
+		frmMediaVault.setBounds(100, 100, 1009, 602);
+		frmMediaVault.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmMediaVault.getContentPane().add(menuBar, BorderLayout.NORTH);
+		
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+		
+		JMenuItem mntmNew = new JMenuItem("New");
+		mnFile.add(mntmNew);
 	}
 
 }
